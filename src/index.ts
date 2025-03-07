1ca6916e008562a716f1ebc277723904a8cbf7be
Leo Sin <leo.sin@my.cityu.edu.hk>
2025-03-06 22:17:18 -0500

    build: expose Number

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export type { Multiply } from './Arithmetic/Multiplication'
 export type { Divide, Mod } from './Arithmetic/Division'
 export type { Pow } from './Arithmetic/Exponentiation'
 export * from "./Arithmetic/Digit";
-export type { Negate, Abs, IsPositive, IsNegative, Compare, IsOdd, IsEven, IsInt, IsNotInt } from './Arithmetic/Number'
-export type { Gt, Lt, Eq, GtOrEq, LtOrEq, Max, Min } from './Arithmetic/Number'
+export * from "./Arithmetic/Number";
 export type { Bit, And, Or, Xor, Not } from './Arithmetic/Bit'
 export * from "./util";
